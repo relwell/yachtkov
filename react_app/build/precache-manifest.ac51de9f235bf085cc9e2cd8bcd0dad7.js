d0fed0cdf2cdf13b7bc927117e0ac1007941fd9f
Robert Elwell <robert.elwell@gmail.com>
2019-04-02 19:43:46 -0400

    add build

---
+++ react_app/build/precache-manifest.ac51de9f235bf085cc9e2cd8bcd0dad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09392f155593c448557179f1da931d36",
+    "url": "/static/media/yacht.09392f15.gif"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c528a483979bb2c582b2",
+    "url": "/static/js/main.991b8057.chunk.js"
+  },
+  {
+    "revision": "efb6fda5cc1747bdc67a",
+    "url": "/static/js/2.f7379878.chunk.js"
+  },
+  {
+    "revision": "c528a483979bb2c582b2",
+    "url": "/static/css/main.f4a50ebc.chunk.css"
+  },
+  {
+    "revision": "046fb2d20d6a633679d749068cb5c9ad",
+    "url": "/index.html"
+  }
+];
